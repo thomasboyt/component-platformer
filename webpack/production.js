e9d62f7b6dc44916dae27386ef48c4b806277e47
Thomas Boyt <me@thomasboyt.com>
2016-07-30 03:01:05 -0400

    Remove raven/secret since I'm not using it

--- webpack/production.js
+++ webpack/production.js
@@ -2,7 +2,6 @@ var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
 
 var config = require('./base');
-var secret = require('../secret');
 
 var execSync = require('child_process').execSync;
 var sha = execSync('git rev-parse --short HEAD', {encoding: 'utf8'}).trim();
@@ -12,7 +11,6 @@ module.exports = webpackMerge(config, {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"',
-        RAVEN_DSN: `"${secret.ravenDSNPublic}"`,
         BUILD_SHA: `"${sha}"`,
       }
     }),
