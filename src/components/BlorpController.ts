c4fef4e809ca1de2cf5ecd258eecb374f406b044
Thomas Boyt <me@thomasboyt.com>
2016-08-04 11:55:59 -0400

    Remove player-following logic in Blorp since it's not really fun

--- src/components/BlorpController.ts
+++ src/components/BlorpController.ts
@@ -47,23 +47,10 @@ export default class BlorpController extends Component<Options> {
     const anim = this.getComponent(AnimationManager);
 
     // follow player if they're on the same general y range and within the patrol boundaries
-    const player = this.world.getComponent(WorldManager).player!;
-    const playerCenter = player.getComponent(Physical).center;
-
-    const xDiff = playerCenter.x - phys.center.x;
-    const yDiff = playerCenter.y - phys.center.y;
-
-    if (playerCenter.x >= this.patrolBounds[0] &&
-        playerCenter.y <= this.patrolBounds[1] &&
-        Math.abs(yDiff) < 20) {
-      this.walkingRight = xDiff > 0;
-
-    } else {
-      if (phys.center.x > this.patrolBounds[1]) {
-        this.walkingRight = false;
-      } else if (phys.center.x < this.patrolBounds[0]) {
-        this.walkingRight = true;
-      }
+    if (phys.center.x > this.patrolBounds[1]) {
+      this.walkingRight = false;
+    } else if (phys.center.x < this.patrolBounds[0]) {
+      this.walkingRight = true;
     }
 
     const walkDirection = this.walkingRight ? 1 : -1;
